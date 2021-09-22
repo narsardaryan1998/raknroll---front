7c04db3d9ed186f1272f68a2c90ea8365a6be42d
sardaryan1998 <narek@eocenterprise.com>
2021-09-22 17:54:32 +0400

    Add parallax effects

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
-    adas
+    <div style="height: 500px"></div>
+    <kinesis-container
+      event="scroll">
+      <kinesis-element
+        class="ggtt"
+        tag="img"
+        :src="'https://dmzn2b8hkpq8b.cloudfront.net/images/products/515x515/S284986.jpg'"
+        :strength="100"
+        type="translate"/>
+    </kinesis-container>
+    <div style="height: 500px"></div>
 
   </div>
 </template>
+<script>
+  import {KinesisContainer, KinesisElement} from 'vue-kinesis'
+
+  export default {
+    components: {
+      KinesisContainer,
+      KinesisElement,
+    },
+  }
+</script>
+
+<style scoped>
+  .ggtt {
+    padding-left: 150px;
+  }
+</style>
