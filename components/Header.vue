0144b1adf12e077c72b29fc4949faa6603c5cbe0
sardaryan1998 <narek@eocenterprise.com>
2021-09-22 16:59:18 +0400

    Change background image

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header id="header" class="clear d-flex justify-content-between font-family-jura">aasd</header>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
