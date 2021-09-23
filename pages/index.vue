663993874f6ae727696f19cbce48cf6c3669bc3c
Narek <sardaryan17@gmail.com>
2021-09-23 12:15:05 +0400

    Add animation aon

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,41 @@
 <template>
   <div>
-    <div style="height: 500px"></div>
     <kinesis-container
       event="scroll">
-      <kinesis-element
-        class="ggtt"
-        tag="img"
-        :src="'https://dmzn2b8hkpq8b.cloudfront.net/images/products/515x515/S284986.jpg'"
-        :strength="100"
-        type="translate"/>
+      <div data-aos="flip-down" data-aos-duration="1000">
+        <kinesis-element
+          class="c-home_hero-primary_image_container"
+          tag="img"
+          :src="'https://cdn.crello.com/api/media/medium/275136968/stock-photo-glass-beer-foam-plate-red'"
+          :strength="100"
+          type="translate"/>
+      </div>
     </kinesis-container>
-    <div style="height: 500px"></div>
-
   </div>
 </template>
 <script>
-  import {KinesisContainer, KinesisElement} from 'vue-kinesis'
+import {KinesisContainer, KinesisElement} from 'vue-kinesis'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-  export default {
-    components: {
-      KinesisContainer,
-      KinesisElement,
-    },
+export default {
+  components: {
+    KinesisContainer,
+    KinesisElement,
+  },
+  mounted() {
+    AOS.init({})
   }
+}
 </script>
 
 <style scoped>
-  .ggtt {
-    padding-left: 150px;
-  }
+.ggtt {
+  padding-left: 150px;
+}
+
+.c-home_hero-primary_image_container {
+  margin-left: 15vw;
+  width: 36.5625vw;
+}
 </style>
