b58105469a72aba08298e4ec234598195af753fc
sardaryan1998 <narek@eocenterprise.com>
2021-12-09 18:03:43 +0400

    Make public contact page

--- pages/delivery-and-payment.vue
+++ pages/delivery-and-payment.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="deliveryAndPayment" class="text-center">
-    <client-only>
-      <div class="pages_top_with_animation d-flex align-center justify-center pt-10">
-        <div class="mr-3"
-             data-aos="fade-right"
-             data-aos-duration="1000">
-          <hr>
-        </div>
-        <div
-          data-aos="fade-up"
-          data-aos-duration="1000">
-          <div class="component-top-header"
-               data-aos="fade-up"
+    <div class="deliveryAndPayment_top-section">
+      <client-only>
+        <div class="pages_top_with_animation d-flex align-center justify-center pt-10">
+          <div class="mr-3"
+               data-aos="fade-right"
                data-aos-duration="1000">
-            <div class="ml-4 black-text-shadow black--text white-brush-background font-caveat">
-              <p class="py-8 px-sm-16 px-8"
+            <hr>
+          </div>
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1000">
+            <div class="component-top-header"
                  data-aos="fade-up"
-                 data-aos-delay="300" data-aos-duration="1000"> {{ $t('menuLinks.delivery-and-payment') }}</p>
+                 data-aos-duration="1000">
+              <div class="ml-4 black-text-shadow black--text white-brush-background font-caveat">
+                <p class="py-8 px-sm-16 px-8"
+                   data-aos="fade-up"
+                   data-aos-delay="300" data-aos-duration="1000"> {{ $t('menuLinks.delivery-and-payment') }}</p>
+              </div>
             </div>
           </div>
+          <div class="ml-3"
+               data-aos="fade-left"
+               data-aos-duration="1000">
+            <hr>
+          </div>
         </div>
-        <div class="ml-3"
-             data-aos="fade-left"
-             data-aos-duration="1000">
-          <hr>
-        </div>
-      </div>
-    </client-only>
+      </client-only>
+    </div>
+    <div class="deliveryAndPayment_content">
+      {{ $t('menuLinks.delivery-and-payment') }}
+    </div>
   </div>
 </template>
 
---
+++ pages/public-contact.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="publicContact">
+    <div class="publicContact_top-section">
+      <client-only>
+        <div class="pages_top_with_animation d-flex align-center justify-center pt-10">
+          <div class="mr-3"
+               data-aos="fade-right"
+               data-aos-duration="1000">
+            <hr>
+          </div>
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1000">
+            <div class="component-top-header"
+                 data-aos="fade-up"
+                 data-aos-duration="1000">
+              <div class="ml-4 black-text-shadow black--text white-brush-background font-caveat">
+                <p class="py-8 px-sm-16 px-8"
+                   data-aos="fade-up"
+                   data-aos-delay="300" data-aos-duration="1000"> {{ $t('publicDocumentPageContent.header') }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="ml-3"
+               data-aos="fade-left"
+               data-aos-duration="1000">
+            <hr>
+          </div>
+        </div>
+      </client-only>
+    </div>
+    <div class="publicContact_content container-padding margin-top-6vh">
+      <div class="font-weight-black">
+        <span v-html="$t('publicDocumentPageContent.div1')"></span>
+      </div>
+      <div class="margin-top-4vh">
+        <span v-html="$t('publicDocumentPageContent.div2')"></span>
+      </div>
+      <div class="margin-top-4vh">
+        <span class="font-weight-black" v-html="$t('publicDocumentPageContent.div3')"></span>
+        <span v-html="$t('publicDocumentPageContent.div4')"></span>
+      </div>
+      <div class="margin-top-4vh">
+        <span class="font-weight-black" v-html="$t('publicDocumentPageContent.div5')"></span>
+        <span v-html="$t('publicDocumentPageContent.div6')"></span>
+      </div>
+      <div class="margin-top-4vh">
+        <span class="font-weight-black" v-html="$t('publicDocumentPageContent.div7')"></span>
+        <span v-html="$t('publicDocumentPageContent.div8')"></span>
+      </div>
+      <div class="margin-top-4vh">
+        <span class="font-weight-black" v-html="$t('publicDocumentPageContent.div9')"></span>
+        <span v-html="$t('publicDocumentPageContent.div10')"></span>
+      </div>
+      <div class="margin-top-4vh">
+        <span class="font-weight-black" v-html="$t('publicDocumentPageContent.div11')"></span>
+        <span v-html="$t('publicDocumentPageContent.div12')"></span>
+      </div>
+      <div class="margin-top-4vh">
+        <span class="font-weight-black" v-html="$t('publicDocumentPageContent.div13')"></span>
+        <span v-html="$t('publicDocumentPageContent.div14')"></span>
+      </div>
+      <div class="margin-top-4vh">
+        <span class="font-weight-black" v-html="$t('publicDocumentPageContent.div15')"></span>
+        <span v-html="$t('publicDocumentPageContent.div16')"></span>
+      </div>
+      <div class="margin-top-4vh">
+        <span class="font-weight-black" v-html="$t('publicDocumentPageContent.div17')"></span>
+        <span v-html="$t('publicDocumentPageContent.div18')"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "publicContact"
+}
+</script>
+
+<style scoped>
+#publicContact {
+  margin-top: 18vh;
+}
+</style>
